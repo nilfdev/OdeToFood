7824090e5628c396c271c5a4dc438d68efc76986
i.v.golovach <i.v.golovach@hotmail.com>
2018-09-13 22:13:06 +0300

    added ef storage

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+	public class SqlRestaurantData : IRestaurantData
+	{
+		private OdeToFoodDbContext _context;
+
+		public SqlRestaurantData(OdeToFoodDbContext context)
+		{
+			_context = context;
+		}
+
+		public Restaurant Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
